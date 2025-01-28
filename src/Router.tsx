7c76a652223447a912639163537553fb1f4282a0
Coovi <hellocoovi@gmail.com>
2025-01-28 13:16:38 +0900

    🎯📦👷 github action 세팅 && 라우트 세팅 - ci 파일 작성 - 기본적인 라우트 설정   -> basename 설정으로 동일한 경로 노출 - vite 설정에서 base 추가로 문제 없이 배포되게

---
+++ src/Router.tsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from "react-router";
+import Home from "./routes/Home";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route index element={<Home />} />
+    </Routes>
+  );
+}
---
+++ src/routes/Home.tsx
@@ -0,0 +1,8 @@
+/**  */
+export default function Home() {
+  return (
+    <div>
+      <h2>This_is_Home</h2>
+    </div>
+  );
+}
